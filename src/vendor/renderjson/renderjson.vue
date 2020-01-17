a894185521c97056a96af59fb3dbd7b520c80ddc
Mo Morsi <mo@morsi.org>
2020-01-16 23:05:25 -0500

    Add vendored renderjson.vue module

---
+++ src/vendor/renderjson/renderjson.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-bind:id="id" />
+</template>
+
+<script>
+import style from './renderjson.css'
+import renderjson from './renderjson.js'
+
+renderjson.set_show_to_level(1);
+
+export default {
+  name: 'renderjson',
+
+  props : {
+    data : {}
+  },
+
+  computed : {
+    id : function(){
+      return "renderjson-" + Math.random();
+    }
+  },
+
+  watch : {
+    data : function(){
+      var div = document.getElementById(this.id);
+      var rendered = renderjson(this.data);
+
+      div.innerHTML = "";
+      div.appendChild(rendered)
+    }
+  },
+
+  mounted : function(){
+    var div = document.getElementById(this.id);
+    var rendered = renderjson(this.data);
+    div.appendChild(rendered)
+  },
+
+  beforeDestroy : function(){
+  },
+}
+</script>
