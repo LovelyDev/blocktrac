5e3b72f6d68be6ec34f16c55a3464ddbc1ad14ab
Mo Morsi <mo@morsi.org>
2020-01-16 23:05:07 -0500

    Continue implementing filter tester

--- src/components/FilterTester.vue
+++ src/components/FilterTester.vue
@@ -1,23 +1,48 @@
 <template>
   <table>
-    <tr v-for="test in tests" :key="test.id">
-      <td :title="test.json">{{test.name}}</td>
-      <td>
-        <img style="width: 22px"
-              :src="success ? '../assets/green-check.svg' :
-                              '../assets/red-x.svg'" />
+    <tr>
+      <td colspan="2" style="text-align: center">
+        <u><b>{{jsonpath}}</b></u>
       </td>
     </tr>
+
+    <template v-for="test in tests">
+      <tr :key="test.id"
+        @click="toggle_details(test)"
+          class="test_results">
+        <td>{{test.name}}</td>
+        <td class="test_glyph">
+          <img width=15
+               :src="result_glyph(test)" />
+        </td>
+      </tr>
+
+      <tr :key="test.id + '_details'"
+        v-show="details[test.id]"
+          class="test_details">
+        <td colspan="2">
+          <renderjson :data="test.json" />
+        </td>
+      </tr>
+    </template>
   </table>
 </template>
 
 <script>
+import renderjson from '../vendor/renderjson/renderjson.vue'
 var ServerLoader = require('../mixins/server_loader').default
+
+var jsonpath = require('../vendor/jsonpath')
+jsonpath.scope({parseInt: parseInt, parseFloat: parseFloat})
 
 export default {
   name: 'FilterTester',
 
   mixins : [ServerLoader],
+
+  components : {
+    renderjson
+  },
 
   props : {
     "filter" : Object
@@ -25,19 +50,70 @@ export default {
 
   data : function(){
     return {
-      tests : []
+         tests : [],
+       details : {},
+       results : {},
+      jsonpath : ''
     };
   },
 
+  methods : {
+    toggle_details : function(test){
+      this.details[test.id] = !this.details[test.id]
+    },
+
+    success : function(test){
+      return this.results[test.id];
+    },
+
+    result_glyph : function(test){
+      if(this.success(test))
+        return require('../assets/green-check.svg');
+      else
+        return require('../assets/red-x.svg');
+    }
+  },
+
   created : function(){
+    if(this.filter.template)
+      this.load_templates(function(templates){
+        var template  = templates[this.filter.template]
+        var _jsonpath = template.jsonpath;
+        this.filter.params.forEach(function(param, p){
+          _jsonpath = _jsonpath.replace("PARAM_" + (p+1), param);
+        });
+        this.jsonpath = _jsonpath;
+      }.bind(this));
+
+    else
+      this.jsonpath = this.filter.jsonpath;
+
     this.load_filter_tests(function(tests){
       tests.forEach(function(test){
+        Object.freeze(test);
+
         this.tests.push(test);
-      });
+        this.$set(this.details, test.id, false);
+
+        var result = jsonpath.query(test.json, this.jsonpath).length != 0;
+        this.$set(this.results, test.id, result);
+      }.bind(this));
     }.bind(this));
   }
 }
 </script>
 
 <style scoped>
+table{
+  width: 100%;
+  table-layout: fixed;
+}
+
+.test_results{
+  cursor: pointer;
+}
+
+.test_glyph{
+  text-align: right;
+}
 </style>
