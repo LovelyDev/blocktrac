e96cfc8ce8a61743f41f14d3456eeb1ea755ab63
Mo Morsi <mo@morsi.org>
2020-05-01 15:15:16 -0400

    Profile page mobile styling

--- src/Profile.vue
+++ src/Profile.vue
@@ -5,61 +5,79 @@
 
       <b-container id="profile_content">
         <b-row class="header_row">
-          <b-col>
+          <b-col class="label">
             My profile
           </b-col>
         </b-row>
 
         <b-row class="input_row">
-          <b-col>
+          <b-col  class="label" cols=4>
             Email address
           </b-col>
 
-          <b-col>
+          <b-col class="value">
             {{email}}
           </b-col>
 
-          <b-col class="action">
+          <b-col v-if="mq_gt_sm"
+                 class="action" cols=2>
             Update
+          </b-col>
+
+          <b-col v-else
+                 class="action" cols="1">
+            <img src="./assets/pencil-blue.svg" />
           </b-col>
         </b-row>
 
         <b-row class="input_row">
-          <b-col>
+          <b-col class="label" cols=4>
             Password
           </b-col>
 
-          <b-col>
+          <b-col class="value">
            *****
           </b-col>
 
-          <b-col class="action">
+          <b-col v-if="mq_gt_sm"
+                 class="action" cols=2>
             Change Password
+          </b-col>
+
+          <b-col v-else
+                 class="action" cols=1>
+            <img src="./assets/pencil-blue.svg" />
           </b-col>
         </b-row>
 
         <b-row class="header_row">
-          <b-col>
+          <b-col class="label">
             Plan details
           </b-col>
         </b-row>
 
         <b-row class="input_row">
-          <b-col>
+          <b-col class="label" cols=4>
             Plan details
           </b-col>
 
-          <b-col id="membership_level">
+          <b-col id="membership_level" class="value">
             {{membership_level}}
           </b-col>
 
-          <b-col class="action">
+          <b-col v-if="mq_gt_sm"
+                 class="action" cols=2>
             Change plan
+          </b-col>
+
+          <b-col v-else
+                 class="action" cols=1>
+            <img src="./assets/pencil-blue.svg" />
           </b-col>
         </b-row>
 
         <b-row class="header_row">
-          <b-col>
+          <b-col class="label">
             Billing
           </b-col>
         </b-row>
@@ -91,10 +109,16 @@ export default {
 
 <style scoped>
 #profile{
-  width: 50%;
+  width: 70%;
   margin: auto;
   margin-top: 20px;
   font-family: var(--theme-font4);
+}
+
+#main_layout.md #profile,
+#main_layout.sm #profile,
+#main_layout.xs #profile{
+  width: unset;
 }
 
 #profile h1{
@@ -105,6 +129,26 @@ export default {
   background-color: white;
   border: 1px solid #ededed;
   padding: 25px;
+  max-width: unset;
+}
+
+#main_layout.md #profile_content,
+#main_layout.sm #profile_content,
+#main_layout.xs #profile_content{
+  padding: 10px;
+}
+
+.label{
+  padding: 0;
+}
+
+#main_layout.md .label,
+#main_layout.sm .label,
+#main_layout.xs .label,
+#main_layout.md .value,
+#main_layout.sm .value,
+#main_layout.xs .value{
+  font-size: 3.8vw;
 }
 
 .header_row{
@@ -114,12 +158,20 @@ export default {
   font-weight: bold;
 }
 
+#main_layout.md .header_row,
+#main_layout.sm .header_row,
+#main_layout.xs .header_row{
+  margin: 10px 0;
+}
+
 .input_row{
   margin: 20px;
 }
 
-.input_row .col{
-  padding: 10px;
+#main_layout.md .input_row,
+#main_layout.sm .input_row,
+#main_layout.xs .input_row{
+  margin: 20px 0;
 }
 
 .action{
