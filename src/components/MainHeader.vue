bbdc2f50b96472145c27c558cc8186dbc16b8e2f
Mo Morsi <mo@morsi.org>
2020-04-30 22:36:05 -0400

    Extract HamburgerNav to its own component

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -14,25 +14,9 @@
       </router-link>
     </div>
 
-    <div v-if="mq_lt_lg">
-      <img id="main_hamburger_open_icon"
-           src="../assets/hamburger.svg"
-           @click="hamburger_visible = true"/>
 
-      <transition name="fade">
-        <div id="main_hamburger" v-show="hamburger_visible">
-          <div id="hide_hamburger"
-               @click="hamburger_visible = false">
-            <img id="main_hamburger_close_icon"
-                 src="../assets/x-white.svg" />
-          </div>
-
-          <MainNav hamburger @nav="hamburger_visible = false"/>
-        </div>
-      </transition>
-    </div>
-
-    <MainNav v-else />
+    <HamburgerNav v-if="mq_lt_lg" />
+    <MainNav      v-else />
 
     <LoginModal />
     <ForgotPasswordModal />
@@ -43,6 +27,7 @@
 
 <script>
 import MainNav             from './MainNav'
+import HamburgerNav        from './HamburgerNav'
 
 import LoginModal          from './modals/Login'
 import ForgotPasswordModal from './modals/ForgotPassword'
@@ -54,17 +39,12 @@ export default {
 
   components : {
     MainNav,
+    HamburgerNav,
     LoginModal,
     ForgotPasswordModal,
     RegistrationModal,
     RegisterFilterModal
-  },
-
-  data : function(){
-    return {
-      hamburger_visible : false
-    };
-  },
+  }
 }
 </script>
 
@@ -104,35 +84,5 @@ export default {
 
 #main_title2{
   color: var(--theme-color1);
-}
-
-#main_hamburger_open_icon{
-  width: 25px;
-  cursor: pointer;
-}
-
-#main_layout.xs #main_hamburger_open_icon{
-  width: 15px;
-}
-
-#main_hamburger{
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 3;
-
-  background-color: var(--theme-color1);
-  padding: 20px;
-}
-
-#main_hamburger_close_icon{
-  width: 25px;
-}
-
-#hide_hamburger{
-  cursor: pointer;
-  text-align: right;
 }
 </style>
