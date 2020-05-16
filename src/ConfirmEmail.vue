4507f113e917885835ab8fe1adffcf985c8f6173
Mo Morsi <mo@morsi.org>
2020-05-16 03:53:09 -0400

    Stub out ConfirmEmail and ResetPassword pages

---
+++ src/ConfirmEmail.vue
@@ -0,0 +1,5 @@
+// TODO:
+// read code from url (route based prop),
+// invoke PUT /confirm w/ code,
+// alert success / error,
+// redirect to /txs
---
+++ src/ResetPassword.vue
@@ -0,0 +1,6 @@
+// TODO:
+// read code from url (route based prop),
+// render form w/ password / confirm password,
+// invoke PUT /reset w/ password and code,
+// alert success / error,
+// redirect to /txs
