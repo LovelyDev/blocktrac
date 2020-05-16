bc4e25454126bf114fdab5a4140db3277aca79d6
Mo Morsi <mo@morsi.org>
2020-05-16 03:52:39 -0400

    Add mandatory Terms of Service agreement checkbox to registration

--- src/components/forms/Registration.vue
+++ src/components/forms/Registration.vue
@@ -51,7 +51,11 @@
     </tr>
 
     <tr>
-      <td>TODO: Agree to terms of service checkbox (with link to ToS page and validation checked)</td>
+      <td colspan="2" id="tos_agree">
+        <b-form-checkbox v-model="tos_agree">
+        I agree to the <router-link to="/terms">Terms of Service</router-link>
+        </b-form-checkbox>
+      </td>
     </tr>
 
     <tr>
@@ -74,19 +78,32 @@ export default {
 
   mixins : [Authentication, Validator],
 
+  data : function(){
+    return {
+      tos_agree : false
+    }
+  },
+
   computed : {
     is_valid : function(){
-      return this.have_email     &&
-            !this.invalid_email  &&
-             this.have_passwords &&
-            !this.invalid_passwords;
+      return this.have_email        &&
+            !this.invalid_email     &&
+             this.have_passwords    &&
+            !this.invalid_passwords &&
+             this.tos_agree;
     }
   }
 }
 </script>
 
 <style scoped>
+#tos_agree{
+  text-align: right;
+  padding-bottom: 10px;
+}
+
 #existing_account{
   cursor: pointer;
+  text-align: right;
 }
 </style>
