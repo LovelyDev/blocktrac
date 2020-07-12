5605d9c49fceb98298f71cc294d09c3f972f7ed0
Mo Morsi <mo@morsi.org>
2020-07-12 08:58:03 -0400

    Fix AccountDetail 'account' param, 'text' does not apply here

--- src/components/AccountDetail.vue
+++ src/components/AccountDetail.vue
@@ -33,10 +33,8 @@ export default {
     },
 
     account : {
-       text : {
-         type : String,
-         required : true
-       }
+       type : String,
+       required : true
     }
   }
 }
