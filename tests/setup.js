db133f6341092aed037b6a4ae4c3d6fcf6ddf960
Mo Morsi <mo@morsi.org>
2021-03-25 20:49:01 -0400

    Setup tests in same manner as main application

--- tests/setup.js
+++ tests/setup.js
@@ -1,16 +1,46 @@
 import { createLocalVue } from "@vue/test-utils"
-import Vuex from 'vuex'
-import Multiselect from 'vue-multiselect'
-import NetworkConnection from '../src/plugins/network-connection'
-
 const localVue = createLocalVue();
+
+///
+
+import Vuex from 'vuex'
 localVue.use(Vuex)
+
+///
+
+import Multiselect from 'vue-multiselect'
 localVue.component('multiselect', Multiselect)
+
+///
+
+import NetworkConnection from '../src/plugins/network-connection'
 localVue.use(NetworkConnection)
 
-import { store } from '../src/store'
+///
+
+import VueReactiveCookie from 'vue-reactive-cookie'
+localVue.use(VueReactiveCookie, {convertJSON : true})
+
+///
+
+import BootstrapVue from 'bootstrap-vue'
+
+localVue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+///
+
+import VueRouter from 'vue-router'
+localVue.use(VueRouter)
+
+///
+
+import { router } from '../src/router'
+import { store  } from '../src/store'
 
 export default {
   localVue,
-  store
+  store,
+  router
 }
