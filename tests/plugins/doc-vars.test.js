466271f3f0c645a6186ec9b9515d9559fe289dab
Mo Morsi <mo@morsi.org>
2021-03-27 10:17:31 -0400

    More tests (doc-vars)

--- tests/plugins/doc-vars.test.js
+++ tests/plugins/doc-vars.test.js
@@ -1,4 +1,11 @@
+import {create_vue} from '../setup'
+import network_config from '../../src/config/network'
+
 describe("doc-vars plugin", () => {
-  test.todo("sets document.title")
+  it("sets document.title", () => {
+    create_vue()
+    expect(document.title).toEqual(network_config.APP_NAME);
+  })
+
   test.todo("sets favicon.href")
 })
