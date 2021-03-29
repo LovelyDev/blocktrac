85cab898482a9510f823f47bdd5222e5ca036276
Mo Morsi <mo@morsi.org>
2021-03-29 13:42:58 -0400

    Implement some FilterDetails tests

--- tests/FilterDetails.test.js
+++ tests/FilterDetails.test.js
@@ -1,7 +1,52 @@
+import {mount_vue}    from './setup'
+import {load_fixture} from './stubs'
+
+import FilterDetails from '../src/FilterDetails.vue'
+
+///
+
+// Filter 'retrieved' from the server
+const filters = load_fixture('filters');
+
+// Filters with sinks
+const filters_with_sinks = filters.filter((f) => f.Sinks && f.Sinks.length > 0)
+
+// Filters without sinks
+const filters_without_sinks = filters.filter((f) => !f.Sinks || f.Sinks.length == 0)
+
+// Filter matches 'retrieved' from server
+const filter_matches = load_fixture('filter_matches');
+
+// Helper to setup the base component
+function filter_details(opts){
+  // Stub server_api call
+  const server_api= {
+    methods : {
+      load_filter : function() {
+        this.active_filter = (opts || {}).active_filter || filters[0]
+      },
+
+      load_filter_matches : function() {
+        this.filter_matches = (opts || {}).matches || []
+      },
+    }
+  };
+
+  // Create new options w/ the stubbed server api mixin
+  const mopts = Object.assign({}, { mixins : [server_api] }, opts)
+
+  // create and return the component
+  return mount_vue(FilterDetails, mopts)
+}
+
+///
+
 describe("FilterDetails Page", () => {
   describe("dom", () => {
     describe("no filter_matches", () => {
-      test.todo("renders no matches content")
+      it("renders no matches content", () => {
+        expect(filter_details().find("#no_matches_text").exists()).toBe(true)
+      })
 
 
       describe("#notifications_text", () => {
@@ -21,29 +66,52 @@ describe("FilterDetails Page", () => {
   })
 
   describe("computed", () => {
-    describe("#have_sinks", () => {
+    describe("have_sinks", () => {
       describe("no active filter", () => {
-        test.todo("is false")
+        it("is false", () => {
+          const fd = mount_vue(FilterDetails, {
+            mixins : [{
+              methods : {
+                load_filter : function() {},
+                load_filter_matches : function() {}
+              }
+            }]
+          })
+
+          expect(fd.vm.have_sinks).toBe(false)
+        })
       })
 
       describe("no active filter sinks (falsy)", () => {
-        test.todo("is false")
+        it("is false", () => {
+          const fd = filter_details();
+                fd.vm.active_filter = filters_without_sinks[0];
+          expect(fd.vm.have_sinks).toBe(false)
+        })
       })
 
       describe("no active filter sinks (.length == 0)", () => {
-        test.todo("is false")
+        it("is false", () => {
+          const fd = filter_details();
+                fd.vm.active_filter = Object.assign({}, filters_with_sinks[0], {Sinks : []})
+          expect(fd.vm.have_sinks).toBe(false)
+        })
       })
 
       describe("active filter associated with sinks", () => {
-        test.todo("is true")
+        it("is true", () => {
+          const fd = filter_details();
+                fd.vm.active_filter = filters_with_sinks[0];
+          expect(fd.vm.have_sinks).toBe(true)
+        })
       })
     })
 
-    describe("#sinks_text", () => {
+    describe("sinks_text", () => {
       test.todo("is comma seperated list of filter sinks' types/targets")
     })
 
-    describe("#match_history", () => {
+    describe("match_history", () => {
       test.todo("is config filter_match_history")
     })
   })
