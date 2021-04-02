77080ba6c2675412d9a9e73a1c6e152a9fe997c6
Mo Morsi <mo@morsi.org>
2021-04-01 23:37:45 -0400

    Flush out ConfirmEmail page tests

--- tests/ConfirmEmail.test.js
+++ tests/ConfirmEmail.test.js
@@ -1,28 +1,140 @@
-import setup from './setup'
+import flushPromises from 'flush-promises'
+
+import {mount_vue}  from './setup'
 import ConfirmEmail from '../src/ConfirmEmail.vue'
 
 describe("ConfirmEmail page", () => {
+  var server_api;
+
+  beforeEach(function(){
+    // Stubbed server_api call
+    server_api = {
+      methods : {
+        confirm_email : jest.fn()
+      }
+    };
+  })
+
   describe("methods", () => {
     describe("#send_request", () => {
-      test.todo("sends confirm email request")
+      it("sends confirm email request", () => {
+        server_api.methods.confirm_email.mockResolvedValue()
+
+        mount_vue(ConfirmEmail, {
+          mixins : [server_api],
+          propsData : {code : 'code'}
+        })
+
+        expect(server_api.methods.confirm_email).toHaveBeenCalledTimes(1)
+      })
 
       describe("confirm email success", () => {
-        test.todo("alerts success")
-        test.todo("redirects to /txs")
+        beforeEach(function(){
+          server_api.methods.confirm_email.mockResolvedValue(
+              {
+                body : {
+                  error : 'error'
+                }
+              }
+            )
+        })
+
+        it("alerts success", async() => {
+          mount_vue(ConfirmEmail, {
+            mixins : [server_api],
+            propsData : {code : 'code'}
+          })
+
+          await flushPromises();
+
+          expect(window.alert).toHaveBeenCalledTimes(1)
+          expect(window.alert.mock.calls[0][0]).toEqual("Email successfully confirmed, you may now login")
+        })
+
+        it("redirects to /txs", async () => {
+          const confirm_email = mount_vue(ConfirmEmail, {
+            mixins : [server_api],
+            propsData : {code : 'code'}
+          })
+
+          await flushPromises();
+
+          expect(confirm_email.vm.$route.path).toEqual("/txs")
+        })
       })
 
       describe("confirm email failure", () => {
-        test.todo("alerts error")
-        test.todo("redirects to /txs")
+        beforeEach(function(){
+          server_api.methods.confirm_email.mockRejectedValue(
+              {
+                body : {
+                  error : 'error'
+                }
+              }
+            )
+        })
+
+        it("alerts error", async () => {
+          mount_vue(ConfirmEmail, {
+            mixins : [server_api],
+            propsData : {code : 'code'}
+          })
+
+          await flushPromises();
+
+          expect(window.alert).toHaveBeenCalledTimes(1)
+          expect(window.alert.mock.calls[0][0]).toEqual("Could not confirm email: Error")
+        })
+
+        it("redirects to /txs", async () => {
+          const confirm_email = mount_vue(ConfirmEmail, {
+            mixins : [server_api],
+            propsData : {code : 'code'}
+          })
+
+          await flushPromises();
+
+          expect(confirm_email.vm.$route.path).toEqual("/txs")
+        })
       })
     })
   })
 
   describe("#created", () => {
     describe("maintenance_mode", () => {
-      test.todo("navs to maintenance")
+      it("navs to maintenance", () => {
+        var maintenance = {
+          computed : {
+            maintenance_mode : function(){
+              return true;
+            }
+          },
+
+          methods : {
+            nav_to_maintenance : jest.fn()
+          }
+        }
+
+        mount_vue(ConfirmEmail, {
+          mixins : [server_api, maintenance],
+          propsData : {code : 'code'}
+        })
+        expect(maintenance.methods.nav_to_maintenance).toHaveBeenCalledTimes(1);
+      })
     })
 
-    test.todo("sends request")
+    it("sends request", () => {
+      const send_request = {
+        methods : {
+          send_request : jest.fn()
+        }
+      }
+
+      mount_vue(ConfirmEmail, {
+        mixins : [server_api, send_request],
+        propsData : {code : 'code'}
+      })
+      expect(send_request.methods.send_request).toHaveBeenCalledTimes(1)
+    })
   })
 })
