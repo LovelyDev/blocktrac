956c4a60c6d4ac0284bae6eb7af25648938011c7
Mo Morsi <mo@morsi.org>
2021-04-05 01:29:49 -0400

    Flush out some server_api tests

--- tests/mixins/server_api.test.js
+++ tests/mixins/server_api.test.js
@@ -1,27 +1,80 @@
-describe("authentication", () => {
+import {shallow_mount_vue} from '../setup'
+
+import config from '../../src/config/config'
+
+import ServerAPI from '../../src/mixins/server_api'
+
+const Component = {
+  render() {},
+  mixins : [ServerAPI]
+}
+
+describe("server_api", () => {
+  var component
+
+  beforeEach(function(){
+    component = shallow_mount_vue(Component)
+  })
+
   describe("computed", () => {
     describe("#backend_url", () => {
-      test.todo("is config.BACKEND_URL")
+      it("is config.BACKEND_URL", () => {
+        expect(component.vm.backend_url).toEqual(config.BACKEND_URL)
+      })
     })
 
     describe("#templates", () => {
-      test.todo("sets/gets templates")
+      it("sets/gets templates", () => {
+        const templates = ['t1', 't2']
+        component.vm.templates = templates
+        expect(component.vm.templates).toEqual(templates)
+        expect(component.vm.$store.state.templates).toEqual(templates)
+      })
     })
 
     describe("#sinks", () => {
-      test.todo("sets/gets sinks")
+      it("sets/gets sinks", () => {
+        const sinks = ['s1', 's2']
+        component.vm.sinks = sinks
+        expect(component.vm.sinks).toEqual(sinks)
+        expect(component.vm.$store.state.sinks).toEqual(sinks)
+      })
     })
 
     describe("#filters", () => {
-      test.todo("sets/gets filters")
+      it("sets/gets filters", () => {
+        const filters = ['f1', 'f2']
+        component.vm.filters = filters
+        expect(component.vm.filters).toEqual(filters)
+        expect(component.vm.$store.state.filters).toEqual(filters)
+      })
     })
 
     describe("#notifications", () => {
-      test.todo("sets/gets notifications")
+      it("sets/gets notifications", () => {
+        const notifications = ['n1', 'n2']
+        component.vm.notifications = notifications
+        expect(component.vm.notifications).toEqual(notifications)
+        expect(component.vm.$store.state.notifications).toEqual(notifications)
+      })
+    })
+
+    describe("#server_status", () => {
+      it("sets/gets server_status", () => {
+        const server_status = {stat : 'us'}
+        component.vm.server_status = server_status
+        expect(component.vm.server_status).toEqual(server_status)
+        expect(component.vm.$store.state.server_status).toEqual(server_status)
+      })
     })
 
     describe("#active_filter", () => {
-      test.todo("sets/gets active_filter")
+      it("sets/gets active_filter", () => {
+        const active_filter = {active : 'filter'}
+        component.vm.active_filter = active_filter
+        expect(component.vm.active_filter).toEqual(active_filter)
+        expect(component.vm.$store.state.active_filter).toEqual(active_filter)
+      })
     })
   })
 
