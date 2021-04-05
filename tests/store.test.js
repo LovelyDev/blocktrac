97a431383b9ea37a264381d00d98329a71d31c80
Mo Morsi <mo@morsi.org>
2021-04-04 20:03:33 -0400

    Flush out remaining store tests

--- tests/store.test.js
+++ tests/store.test.js
@@ -1,5 +1,7 @@
 import {shallow_mount_vue} from './setup'
 
+import txs_config from '../src/config/txs'
+
 const Component = {
   render() {}
 }
@@ -78,126 +80,194 @@ describe("store", () => {
 
   describe("#update_tx_filter", () => {
     describe("filter is set and is not valid jsonpath", () => {
-      test.todo("does not set filter")
+      it("does not set filter", () => {
+        component.vm.$store.commit('update_tx_filter', '[(')
+        expect(component.vm.$store.state.tx_filter).toEqual('')
+      })
     })
 
-    test.todo("sets filter")
+    it("sets filter", () => {
+      component.vm.$store.commit('update_tx_filter', '$')
+      expect(component.vm.$store.state.tx_filter).toEqual('$')
+
+      component.vm.$store.commit('update_tx_filter', '')
+      expect(component.vm.$store.state.tx_filter).toEqual('')
+    })
 
     describe("filter is set", () => {
-      test.todo("removes txs that do not match filter")
+      it("removes txs that do not match filter", () => {
+        const tx1 = {category : 'PAYMENTS', tx : { id : 'tx1' }};
+        const tx2 = {category : 'OFFERS',   tx : { id : 'tx2' }};
+        component.vm.$store.commit('add_tx', tx1);
+        component.vm.$store.commit('add_tx', tx2);
+        expect(component.vm.$store.state.txs).toEqual([tx2, tx1])
+
+        component.vm.$store.commit('update_tx_filter', "$[?(@.id == 'tx1')]")
+        expect(component.vm.$store.state.txs).toEqual([tx1])
+      })
     })
   })
 
   describe("#add_tx", () => {
     describe("txs are paused", () => {
-      test.todo("does not add tx")
+      it("does not add tx", () => {
+        component.vm.$store.commit("toggle_paused_txs")
+        component.vm.$store.commit('add_tx', {category : 'PAYMENTS'});
+        expect(component.vm.$store.state.txs.length).toBe(0)
+      })
     })
 
     describe("filter is set", () => {
       describe("tx does not match filter", () => {
-        test.todo("does not add tx")
+        it("does not add tx", () => {
+          component.vm.$store.commit('update_tx_filter', "$[?(@.some == 'thing')]")
+          component.vm.$store.commit('add_tx', {category : 'PAYMENTS'});
+          expect(component.vm.$store.state.txs.length).toBe(0)
+        })
       })
     })
 
     describe("tx categories are set and do not include tx category", () => {
-      test.todo("does not add tx")
+      it("does not add tx", () => {
+        component.vm.$store.commit('toggle_tx_category', 'PAYMENTS')
+        component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+        expect(component.vm.$store.state.txs.length).toBe(0)
+      })
     })
 
-    test.todo("sets loading_txs to false")
+    it("prepends tx", () => {
+      const tx = {category : 'OFFERS'}
+      component.vm.$store.commit('add_tx', tx);
+      expect(component.vm.$store.state.txs).toEqual([tx]);
+    })
 
-    test.todo("prepends tx")
-    test.todo("caps number of txs to TX_HISTORY")
-    test.todo("updates 'ALL' tally")
-    test.todo("updates category tally")
+    it("caps number of txs to TX_HISTORY", () => {
+      for(var t = 0; t < txs_config.TX_HISTORY + 1; t += 1)
+        component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+      expect(component.vm.$store.state.txs.length).toEqual(txs_config.TX_HISTORY);
+    })
+
+    it("updates 'ALL' tally", () => {
+      component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+      component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+      component.vm.$store.commit('add_tx', {category : 'PAYMENTS'});
+      expect(component.vm.$store.state.tx_category_tallies['ALL']).toEqual(3);
+    })
+
+    it("updates category tally", () => {
+      component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+      component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+      component.vm.$store.commit('add_tx', {category : 'PAYMENTS'});
+      expect(component.vm.$store.state.tx_category_tallies['OFFERS']).toEqual(2);
+      expect(component.vm.$store.state.tx_category_tallies['PAYMENTS']).toEqual(1);
+    })
   })
 
   describe("#clear_tx_categories", () => {
-    test.todo("empties tx categories")
+    it("empties tx categories", () => {
+      component.vm.$store.commit('toggle_tx_category', 'PAYMENTS')
+      component.vm.$store.commit('clear_tx_categories')
+      expect(component.vm.$store.state.tx_categories.length).toBe(0)
+    })
   })
 
   describe("#toggle_tx_category", () => {
     describe("tx category enabled", () => {
-      test.todo("removes tx category")
+      it("removes tx category", () => {
+        component.vm.$store.commit('toggle_tx_category', 'PAYMENTS')
+        component.vm.$store.commit('toggle_tx_category', 'OFFERS')
+        expect(component.vm.$store.state.tx_categories).toEqual(['PAYMENTS', 'OFFERS'])
+
+        component.vm.$store.commit('toggle_tx_category', 'PAYMENTS')
+        expect(component.vm.$store.state.tx_categories).toEqual(['OFFERS'])
+      })
     })
 
     describe("tx category not enabled", () => {
-      test.todo("adds tx category")
+      it("adds tx category", () => {
+        component.vm.$store.commit('toggle_tx_category', 'OFFERS')
+        expect(component.vm.$store.state.tx_categories).toEqual(['OFFERS'])
+      })
     })
 
-    test.todo("removes txs not in enabled categories")
+    it("removes txs not in enabled categories", () => {
+      const tx1 = {category : 'OFFERS'};
+      const tx2 = {category : 'PAYMENTS'};
+      const tx3 = {category : 'TRUST LINES'};
+      component.vm.$store.commit('add_tx', tx1);
+      component.vm.$store.commit('add_tx', tx2);
+      component.vm.$store.commit('add_tx', tx3);
+
+      component.vm.$store.commit('toggle_tx_category', 'OFFERS')
+      expect(component.vm.$store.state.txs).toEqual([tx1])
+    })
   })
 
   describe("#clear_txs", () => {
-    test.todo("empties txs")
+    it("empties txs", () => {
+      component.vm.$store.commit('add_tx', {category : 'OFFERS'});
+      component.vm.$store.commit('add_tx', {category : 'PAYMENTS'});
+      expect(component.vm.$store.state.txs.length).toBe(2)
+
+      component.vm.$store.commit('clear_txs')
+      expect(component.vm.$store.state.txs.length).toBe(0)
+    })
   })
 
   describe("#set_templates", () => {
-    test.todo("sets templates property")
+    it("sets templates property", () => {
+      component.vm.$store.commit('set_templates', ['t1', 't2'])
+      expect(component.vm.$store.state.templates).toEqual(['t1', 't2'])
+    })
   })
 
   describe("#set_sinks", () => {
-    test.todo("sets sinks property")
+    it("sets sinks property", () => {
+      component.vm.$store.commit('set_sinks', ['s1', 's2'])
+      expect(component.vm.$store.state.sinks).toEqual(['s1', 's2'])
+    })
   })
 
   describe("#set_filters", () => {
-    test.todo("sets filters property")
+    it("sets filters property", () => {
+      component.vm.$store.commit('set_filters', ['f1', 'f2'])
+      expect(component.vm.$store.state.filters).toEqual(['f1', 'f2'])
+    })
   })
 
   describe("#set_filter_matches", () => {
-    test.todo("sets filter_matches property")
+    it("sets filter_matches property", () => {
+      component.vm.$store.commit('set_filter_matches', ['fm1', 'fm2'])
+      expect(component.vm.$store.state.filter_matches).toEqual(['fm1', 'fm2'])
+    })
   })
 
   describe("#set_notifications", () => {
-    test.todo("sets notifications property")
+    it("sets notifications property", () => {
+      component.vm.$store.commit('set_notifications', ['n1', 'n2'])
+      expect(component.vm.$store.state.notifications).toEqual(['n1', 'n2'])
+    })
   })
 
   describe("#set_active_filter", () => {
-    test.todo("sets active_filter property")
+    it("sets active_filter property", () => {
+      component.vm.$store.commit('set_active_filter', {id : 'filter1'})
+      expect(component.vm.$store.state.active_filter).toEqual({id : 'filter1'})
+    })
   })
 
   describe("#set_in_progress_filter", () => {
-    test.todo("sets in_progress_filter property")
+    it("sets in_progress_filter property", () => {
+      component.vm.$store.commit('set_in_progress_filter', {id : 'filter1'})
+      expect(component.vm.$store.state.in_progress_filter).toEqual({id : 'filter1'})
+    })
   })
 
   describe("#clear_in_progress_filter", () => {
-    test.todo("clear in_progress_filter property")
-  })
-
-  describe("#on_open_socket", () => {
-    describe("socket is connected", () => {
-      test.todo("calls callback")
+    it("clear in_progress_filter property", () => {
+      component.vm.$store.commit('set_in_progress_filter', {id : 'filter1'})
+      component.vm.$store.commit('clear_in_progress_filter')
+      expect(component.vm.$store.state.in_progress_filter).toEqual({})
     })
-
-    test.todo("adds callback to socket_open callbacks")
-  })
-
-  describe("#on_socket_message", () => {
-    test.todo("adds callback to socket_msg callbacks")
-  })
-
-  describe("#rm_socket_message", () => {
-    test.todo("removes callback from socket_msg callbacks")
-  })
-
-  describe("on socket open", () => {
-    test.todo("sets isConnected")
-    test.todo("calls socket open callbacks")
-  })
-
-  describe("on socket close", () => {
-    test.todo("sets isConnected to flags")
-  })
-
-  describe("on socket message", () => {
-    describe("callback for message id exists", () => {
-      test.todo("invokes callback")
-      test.todo("deletes callback")
-    })
-
-    test.todo("calls socket message callbacks")
-  })
-
-  describe("on socket reconnect error", () => {
-    test.todo("sets reconnectError")
   })
 })
