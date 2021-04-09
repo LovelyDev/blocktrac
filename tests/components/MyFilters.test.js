2fb9faf28a2e98b310ed1f90f6ff5eac9ff0ab25
Mo Morsi <mo@morsi.org>
2021-04-09 15:02:17 -0400

    Implement some components/MyFilters tests

--- tests/components/MyFilters.test.js
+++ tests/components/MyFilters.test.js
@@ -1,19 +1,85 @@
+import {shallow_mount_vue} from '../setup'
+
+import MyFilters from '../../src/components/MyFilters'
+
 describe("MyFilters", () => {
   describe("dom", () => {
+    describe("#my_filters_anon", () => {
+      describe("logged in", () => {
+        test.todo("is not rendered")
+      })
+
+      describe("not logged in", () => {
+        test.todo("is rendered")
+      })
+    })
+
+    describe("#my_filters_logged_in", () => {
+      describe("logged in", () => {
+        test.todo("is rendered")
+      })
+
+      describe("not logged in", () => {
+        test.todo("is not rendered")
+      })
+    })
+
     describe("#add_new_filter", () => {
       describe("remaining_filters > 0", () => {
-        test.todo("it is rendered")
+        it("is rendered", () => {
+          const mf = shallow_mount_vue(MyFilters, {
+            computed : {
+              auth_token : function(){
+                return 'auth_token';
+              },
+
+              remaining_filters : function(){
+                return 1;
+              }
+            }
+          })
+
+          expect(mf.find("#add_new_filter").exists()).toBe(true)
+        })
       })
 
       describe("remaining_filters == 0", () => {
-        test.todo("it is not rendered")
+        it("is not rendered", () => {
+          const mf = shallow_mount_vue(MyFilters, {
+            computed : {
+              auth_token : function(){
+                return 'auth_token';
+              },
+
+              remaining_filters : function(){
+                return 0;
+              }
+            }
+          })
+
+          expect(mf.find("#add_new_filter").exists()).toBe(false)
+        })
       })
 
       test.todo("launches create_filter modal")
     })
 
     describe("#remaining_filters", () => {
-      test.todo("renders remaining_filters_msg")
+      it("renders remaining_filters_msg", () => {
+        const mf = shallow_mount_vue(MyFilters, {
+          computed : {
+            auth_token : function(){
+              return 'auth_token';
+            },
+
+            remaining_filters_msg : function(){
+              return 'blah BLARG';
+            }
+          }
+        })
+
+        expect(mf.find("#remaining_filters").text()).toEqual("blah BLARG Get a pro account")
+      })
     })
 
     describe("CreateFilterModal", () => {
