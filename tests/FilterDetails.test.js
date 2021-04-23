9291b0aaad0d5dd6cc247cb94b3d6e0914df8521
Mo Morsi <mo@morsi.org>
2021-04-23 13:21:15 -0400

    Update filter_matches fixture to contain server loaded data format
    Previously fixture data was not in format retrieved from server
    
    Updates FilterDetails test to simplify and work with new data format

--- tests/FilterDetails.test.js
+++ tests/FilterDetails.test.js
@@ -1,7 +1,8 @@
 import {
   mount_vue,
+  next_tick,
   flush_promises
-}    from './setup'
+} from './setup'
 
 import {
   stubbed_maintenance_mode as maintenance_mode
@@ -32,7 +33,7 @@ const filter_matches = load_fixture('filter_matches');
 ///
 
 describe("FilterDetails Page", () => {
-  var server_api
+  var server_api, filter_details
 
   beforeEach(function(){
     // Stub server_api call
@@ -47,12 +48,15 @@ describe("FilterDetails Page", () => {
         }),
       }
     };
+
+    filter_details = mount_vue(FilterDetails, {
+      mixins: [server_api]
+    })
   })
 
   describe("dom", () => {
     describe("no filter_matches", () => {
       it("renders no matches content", () => {
-        const filter_details = mount_vue(FilterDetails, {mixins : [server_api]})
         expect(filter_details.find("#no_matches")).toExist()
       })
 
@@ -60,54 +64,47 @@ describe("FilterDetails Page", () => {
       describe("#notifications_text", () => {
         describe("filter associated with sinks", () => {
           it("renders sinks", () => {
-            const filter_details = mount_vue(FilterDetails, {mixins: [server_api]})
             expect(filter_details.find("#get_notifications").text()).toBe("You will get notifications via email demo@blocktr.ac, sms 1112223333");
           })
         })
       })
     })
 
-    describe("#matched_txs_explanation", () => {
-      describe("total_matches > match_history", () => {
-        it("renders match_history text", () => {
-          const filter_details = mount_vue(FilterDetails, 
-            {
-              computed: {
-                match_history: function(){
-                  return 0
-                }
-              }, 
-              mixins: [{
-                methods : {
-                  load_filter : jest.fn(function() {
-                    this.active_filter = filters[0]
-                  }),
-                  load_filter_matches : jest.fn(function() {
-                    this.filter_matches = filter_matches;
-                  }),
-                }
-              }],
-            }
-          );
-          expect(filter_details.find("#match_history").text()).toBe("Only the last 0 matches are shown.");
+    describe("with filter_matches", () => {
+      beforeEach(function(){
+        filter_details = mount_vue(FilterDetails,
+          {
+            computed: {
+              match_history: function(){
+                return 0
+              }
+            },
+            mixins: [{
+              methods : {
+                load_filter : function() {
+                  this.active_filter = filters[0]
+                },
+
+                load_filter_matches : function() {
+                  this.filter_matches = filter_matches;
+                },
+              }
+            }],
+          }
+        );
+      })
+
+      describe("#matched_txs_explanation", () => {
+        describe("total_matches > match_history", () => {
+          it("renders match_history text", () => {
+            expect(filter_details.find("#match_history").text()).toBe("Only the last 0 matches are shown.");
+          })
         })
       })
-    })
 
-    it("renders filter matches", () => {
-      const filter_details = mount_vue(FilterDetails, {
-        mixins: [{
-          methods : {
-            load_filter : jest.fn(function() {
-              this.active_filter = filters[0]
-            }),
-            load_filter_matches : jest.fn(function() {
-              this.filter_matches = filter_matches;
-            }),
-          }
-        }]
-      });
-      expect(filter_details.findAll('.tx_summary_container').length).toBe(filter_matches.length);
+      it("renders filter matches", () => {
+        expect(filter_details.findAll('.tx_summary_container').length).toBe(filter_matches.length);
+      })
     })
   })
 
@@ -115,40 +112,44 @@ describe("FilterDetails Page", () => {
     describe("have_sinks", () => {
       describe("no active filter", () => {
         it("is false", () => {
-          const fd = mount_vue(FilterDetails, {
-            mixins : [{
+          filter_details = mount_vue(FilterDetails, {
+            mixins: [{
               methods : {
-                load_filter : function() {},
-                load_filter_matches : function() {}
+                load_filter : function() {
+                },
+
+                load_filter_matches : function() {
+                  this.filter_matches = []
+                },
               }
             }]
           })
 
-          expect(fd.vm.have_sinks).toBe(false)
+          expect(filter_details.vm.have_sinks).toBe(false)
         })
       })
 
       describe("no active filter sinks (falsy)", () => {
-        it("is false", () => {
-          const filter_details = mount_vue(FilterDetails, {mixins : [server_api]})
+        it("is false", async () => {
           filter_details.vm.active_filter = filters_without_sinks[0];
+          await next_tick(filter_details);
           expect(filter_details.vm.have_sinks).toBe(false)
         })
       })
 
       describe("no active filter sinks (.length == 0)", () => {
-        it("is false", () => {
-          const filter_details = mount_vue(FilterDetails, {mixins : [server_api]})
+        it("is false", async () => {
           filter_details.vm.active_filter =
             Object.assign({}, filters_with_sinks[0], {Sinks : []})
+          await next_tick(filter_details);
           expect(filter_details.vm.have_sinks).toBe(false)
         })
       })
 
       describe("active filter associated with sinks", () => {
-        it("is true", () => {
-          const filter_details = mount_vue(FilterDetails, {mixins : [server_api]})
+        it("is true", async () => {
           filter_details.vm.active_filter = filters_with_sinks[0];
+          await next_tick(filter_details);
           expect(filter_details.vm.have_sinks).toBe(true)
         })
       })
@@ -156,7 +157,6 @@ describe("FilterDetails Page", () => {
 
     describe("sinks_text", () => {
       it("is comma seperated list of filter sinks' types/targets", () => {
-        const filter_details = mount_vue(FilterDetails, {mixins: [server_api]})
         filter_details.setData({active_filter: filters_with_sinks[0]});
         expect(filter_details.vm.sinks_text.split(',').length).toBe(filters_with_sinks[0].Sinks.length);
       })
@@ -164,8 +164,29 @@ describe("FilterDetails Page", () => {
 
     describe("match_history", () => {
       it("is config filter_match_history", () => {
-        const filter_details = mount_vue(FilterDetails, {mixins: [server_api]})
         expect(filter_details.vm.match_history).toBe(ziti.filter_match_history)
+      })
+    })
+
+    describe("filter_match_txs", () => {
+      it("is raw transactions corresponding to filter matches", () => {
+        filter_details = mount_vue(FilterDetails, {
+          mixins: [{
+            methods : {
+              load_filter : function() {},
+
+              load_filter_matches : function() {
+                this.filter_matches = filter_matches;
+              },
+            }
+          }],
+        });
+
+        const expected = filter_matches.map((match) => {
+                           return match.Transaction.raw
+                         });
+
+        expect(filter_details.vm.filter_match_txs).toEqual(expected)
       })
     })
   })
@@ -173,13 +194,11 @@ describe("FilterDetails Page", () => {
   describe("watch", () => {
     describe("route", () => {
       it("loads filter",  async() => {
-        const filter_details = mount_vue(FilterDetails, {mixins: [server_api]});
         filter_details.vm.$router.push({path: "/filter/" + filters[1].id});
         await flush_promises();
         expect(server_api.methods.load_filter).toHaveBeenCalledTimes(2);
       })
       it("loads filter matches", async() => {
-        const filter_details = mount_vue(FilterDetails, {mixins: [server_api]});
         filter_details.vm.$router.push({path: "/filter/" + filters[1].id});
         await flush_promises();
         expect(server_api.methods.load_filter_matches).toHaveBeenCalledTimes(2);
@@ -197,12 +216,10 @@ describe("FilterDetails Page", () => {
     })
 
     it("loads filter", () => {
-      mount_vue(FilterDetails, {mixins : [server_api]})
       expect(server_api.methods.load_filter).toHaveBeenCalledTimes(1);
     })
 
     it("loads filter matches", () => {
-      mount_vue(FilterDetails, {mixins : [server_api]})
       expect(server_api.methods.load_filter_matches).toHaveBeenCalledTimes(1);
     })
   })
