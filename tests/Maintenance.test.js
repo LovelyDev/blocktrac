00e847a83d457cbc2d2f9552ea14e8eb0ad1968d
sanjanadhikari1 <sanjanadhikari1@gmail.com>
2021-04-26 18:16:14 -0400

    complete maintenance test

--- tests/Maintenance.test.js
+++ tests/Maintenance.test.js
@@ -4,12 +4,33 @@ import Maintenance from '../src/Maintenance.vue'
 describe("Maintenance Page", () => {
   describe("computed", () => {
     describe("img", () => {
-      describe("no blockchain configured", () => {
-        test.todo("is /assets/maintenance/blockchain.svg")
+      describe("no xrp configured", () => {
+        it("is /asses/maintenance/blockchain.svg", () => {
+          const maintenance_test = mount_vue(Maintenance,{
+            computed: {
+              no_blockchain_configured: function() {
+                return true;
+              }
+            }
+          });
+          expect(maintenance_test.vm.img).toEqual(require('./assets/maintenance/blockchain.svg'))
+        })
       })
 
       describe("blockchain configured", () => {
-        test.todo("is /assets/maintenance/configured_blockchain_upper.svg")
+        it("is /assets/maintenance/configured_blockchain_upper.svg", () => {
+          const maintenance_test = mount_vue(Maintenance,{
+            computed: {
+              no_blockchain_configured: function() {
+                return false;
+              },
+              configured_blockchain_upper: function(){
+                return "xrp";
+              }
+            }
+          });
+          expect(maintenance_test.vm.img).toEqual(require('./assets/maintenance/xrp.svg'))
+        })
       })
     })
   })
@@ -26,7 +47,6 @@ describe("Maintenance Page", () => {
             }
           }]
         })
-
         expect(maintenance.vm.$route.path).toEqual("/txs")
       })
     })
