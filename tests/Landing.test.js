3591069969ed644db714d38dcd8b6f2a79284afe
sanjanadhikari1 <sanjanadhikari1@gmail.com>
2021-04-26 17:48:35 -0400

    complete test code for the landing page

--- tests/Landing.test.js
+++ tests/Landing.test.js
@@ -1,12 +1,41 @@
+import {
+  mount_vue
+} from './setup';
+
+import Landing from '../src/Landing.vue'
 describe("Landing Page", () => {
+  let con_landing_test, nocon_landing_test;
+  beforeEach(function(){
+    nocon_landing_test = mount_vue(Landing, {
+      computed: {
+        no_blockchain_configured: function(){
+          return true;
+        }
+      }
+    })
+    con_landing_test = mount_vue(Landing, {
+      computed: {
+        no_blockchain_configured: function(){
+          return false;
+        },
+        configured_blockchain_upper: function(){
+          return "xrp";
+        }
+      }
+    })
+  })
   describe("computed", () => {
     describe("#blockchain_text", () => {
       describe("no blockchain configured", () => {
-        test.todo("is 'Blockchain'")
+        it("is 'Blockchain'", () => {
+          expect(nocon_landing_test.vm.blockchain_text).toBe("Blockchain");
+        })
       })
 
       describe("blockchain configured", () => {
-        test.todo("is configured_blockchain_upper")
+        it("is configured_blockchain_upper", () => {
+          expect(con_landing_test.vm.blockchain_text).toBe("xrp")
+        })
       })
     })
   })
@@ -14,11 +43,15 @@ describe("Landing Page", () => {
   describe("methods", () => {
     describe("#img_src", () => {
       describe("no blockchain configured", () => {
-        test.todo("is /assets/landing/blockchain/N.png")
+        it("is /assets/landing/blockchain/N.png", () => {
+          expect(nocon_landing_test.vm.img_src('section1')).toEqual(require('./assets/landing/blockchain/section1.png'))
+        })
       })
 
       describe("blockchain configured", () => {
-        test.todo("is /assets/landing/configured_blockchain/N.png")
+        it("is /assets/landing/configured_blockchain/N.png", () => {
+          expect(con_landing_test.vm.img_src('section1')).toEqual(require('./assets/landing/xrp/section1.png'))
+        })
       })
     })
   })
